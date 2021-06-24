6feb1894374bad0753c49ac638f12a67d1566b84
js-roman <coercitivo2014@gmail.com>
2021-06-24 19:44:32 +0200

    dtag-n-drop finished

--- js/escribirse.js
+++ js/escribirse.js
@@ -1,76 +1,85 @@
+let turObjects;
+let turObjectContainer;
+let cancelChoiceButtonl;
+let destination;
+let placeholders;
+
 window.onload = function() {
-    const turObjects = document.querySelectorAll(".object");
-    // const item = document.querySelector(".item");
-    const object1 = document.querySelector("#object1");
-    const object2 = document.querySelector("#object2");
-    const object3 = document.querySelector("#object3");
-    const object4 = document.querySelector("#object4");
+    turObjects = document.querySelectorAll(".object");
+    turObjectContainer = document.querySelector(".objects");
+    cancelChoiceButton = document.querySelector(".helpChoice");
+    destination = document.querySelector(".destination");
+    placeholders = document.querySelectorAll(".placeholder");
+
     let objID;
-    const placeholders = document.querySelectorAll(".placeholder");
+    start();
 
+    destination.addEventListener("dragover", dragover);
+    destination.addEventListener("drop", dragdrop);
+};
+
+function restart() {
+    turObjectContainer.style.display = "inherit";
+    cancelChoiceButton.innerHTML = "Arrastre una  miniatura aqui";
+    cancelChoiceButton.removeEventListener("click", restart);
+    const item = document.querySelector(".inserted");
+    item.classList.remove("inserted");
+    turObjectContainer.append(item);
+    destination.innerHTML = "I";
+    start();
+}
+
+function start() {
     for (let turObject of turObjects) {
-        console.log(turObject);
-
         turObject.addEventListener("dragstart", dragstart);
         turObject.addEventListener("dragend", dragend);
     }
-    // object1.addEventListener("dragstart", dragstart);
-    // object1.addEventListener("dragend", dragend);
-    // object2.addEventListener("dragstart", dragstart);
-    // object2.addEventListener("dragend", dragend);
+}
 
+function finish() {
+    for (let turObject of turObjects) {
+        turObject.removeEventListener("dragstart", dragstart);
+        turObject.removeEventListener("dragend", dragend);
+    }
+    document.querySelector(".objects").style.display = "none";
+
+    cancelChoiceButton.innerHTML = "Click aqui para cancelar la votación";
+    cancelChoiceButton.addEventListener("click", restart);
+    destination.addEventListener("click", restart);
+}
+
+function dragstart(event) {
+    event.target.classList.add("hold", "hanged");
+    objID = event.target.id;
+
+    destination.classList.add("available");
+}
+
+function dragend(event) {
+    console.log("dragend");
+    console.log(event.target);
+
+    event.target.classList.remove("hold");
+    destination.classList.remove("available");
+}
+
+function dragover(event) {
+    event.preventDefault();
+    console.log("dragover");
+}
+
+function dragdrop(event) {
+    console.log(event.target);
+
+    event.target.classList.remove("hovered");
+    document.getElementById(objID).classList.remove("hide");
+
+    event.target.append(document.getElementById(objID));
+    document.getElementById(objID).classList.add("inserted");
+    document.getElementById(objID).classList.remove("hold");
     for (placeholder of placeholders) {
-        placeholder.addEventListener("dragover", dragover);
-        placeholder.addEventListener("dragenter", dragenter);
-        placeholder.addEventListener("dragleave", dragleave);
-        placeholder.addEventListener("drop", dragdrop);
+        placeholder.classList.remove("available");
     }
 
-    function dragstart(event) {
-        event.target.classList.add("hold", "hanged");
-        objID = event.target.id;
-        console.log(objID);
-
-        for (placeholder of placeholders) {
-            placeholder.classList.add("available");
-        }
-
-        setTimeout(() => {
-            event.target.classList.add("hide");
-            event.target.classList.remove("hanged");
-        }, 0);
-    }
-
-    function dragend(event) {
-        event.target.classList.remove("hold");
-        // event.target.className = "item";
-    }
-
-    function dragover(event) {
-        event.preventDefault();
-    }
-
-    function dragenter(event) {
-        if (!event.target.classList.contains("placeholder")) {
-            return;
-        }
-
-        event.target.classList.add("hovered");
-    }
-
-    function dragleave(event) {
-        event.target.classList.remove("hovered");
-    }
-
-    function dragdrop(event) {
-        event.target.classList.remove("hovered");
-        document.getElementById(objID).classList.remove("hide");
-
-        console.log(event.target);
-        event.target.append(document.getElementById(objID));
-        document.getElementById(objID).classList.add("inserted");
-        for (placeholder of placeholders) {
-            placeholder.classList.remove("available");
-        }
-    }
-};
+    finish();
+}
