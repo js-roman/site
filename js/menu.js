ebf8fa5edf91b98f590b5fea48eb13ea34329664
js-roman <coercitivo2014@gmail.com>
2021-09-21 21:42:57 +0200

    added mobile menu

--- js/menu.js
+++ js/menu.js
@@ -3,3 +3,14 @@ let activeSlideIndex = 0;
 function sliderChoice(slide) {
     activeSlideIndex = slide;
 }
+
+window.onload = function() {
+    const menu_switcher = document.querySelector("#menu_switcher");
+    const menu = document.querySelector(".menu");
+
+    menu_switcher.addEventListener("click", () => {
+        menu.classList.toggle("active");
+        // menu_switcher.style.display = "none";
+        menu_switcher.classList.toggle("active");
+    });
+};
