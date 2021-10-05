0e3a2ebe35b071b9ace1d74d53ab881142b6c979
js-roman <coercitivo2014@gmail.com>
2021-10-05 23:36:18 +0200

    fixed drag and drop

--- js/votacion.js
+++ js/votacion.js
@@ -1,91 +1,108 @@
+document.addEventListener("DOMContentLoaded", votation, false);
 let turObjects;
 let turObjectContainer;
-let cancelChoiceButtonl;
+let cancelChoiceButton;
 let destination;
 let placeholders;
+let clearAll;
+votation();
 
-function restart() {
-    cancelChoiceButton.innerHTML = "Arrastre una  miniatura aqui";
-    cancelChoiceButton.removeEventListener("click", restart);
-    const item = document.querySelector(".inserted");
-    if (item != null) {
-        item.classList.remove("inserted");
-        turObjectContainer.append(item);
-    }
+function votation() {
+    turObjects = document.querySelectorAll(".object");
+    turObjectContainer = document.querySelector(".objects");
+    cancelChoiceButton = document.querySelector(".helpChoiceMouse");
+    destination = document.querySelector(".destination");
+    placeholders = document.querySelectorAll(".placeholder");
 
-    showSidebar();
-
-    destination.innerHTML = "I";
-    const resImgs = document.querySelectorAll(".val");
-    for (res of resImgs) {
-        res.removeAttribute("src");
-    }
-
+    let objID;
     start();
-}
 
-function start() {
-    for (let turObject of turObjects) {
-        turObject.addEventListener("dragstart", dragstart);
-        turObject.addEventListener("dragend", dragend);
-    }
-}
+    destination.addEventListener("dragover", dragover);
+    destination.addEventListener("drop", dragdrop);
 
-function finish() {
-    for (let turObject of turObjects) {
-        turObject.removeEventListener("dragstart", dragstart);
-        turObject.removeEventListener("dragend", dragend);
-    }
-    hideSidebar();
-    cancelChoiceButton.innerHTML = "Click aqui para cancelar la votación";
-    cancelChoiceButton.addEventListener("click", restart);
-    destination.addEventListener("click", restart);
-}
+    clearAll = function restart() {
+        cancelChoiceButton.innerHTML = "Arrastre una  miniatura aqui";
+        cancelChoiceButton.removeEventListener("click", clearAll);
+        const item = document.querySelector(".inserted");
+        if (item != null) {
+            item.classList.remove("inserted");
+            turObjectContainer.append(item);
+        }
 
-function dragstart(event) {
-    event.target.classList.add("hold", "hanged");
-    objID = event.target.id;
+        showSidebar();
 
-    destination.classList.add("available");
-}
+        destination.innerHTML = "I";
+        const resImgs = document.querySelectorAll(".val");
+        for (res of resImgs) {
+            res.removeAttribute("src");
+        }
 
-function dragend(event) {
-    event.target.classList.remove("hold");
-    destination.classList.remove("available");
-}
+        start();
+    };
 
-function dragover(event) {
-    event.preventDefault();
-}
-
-function dragdrop(event) {
-    event.target.classList.remove("hovered");
-    document.getElementById(objID).classList.remove("hide");
-
-    event.target.append(document.getElementById(objID));
-    document.getElementById(objID).classList.add("inserted");
-    document.getElementById(objID).classList.remove("hold");
-    for (placeholder of placeholders) {
-        placeholder.classList.remove("available");
+    function start() {
+        for (let turObject of turObjects) {
+            turObject.addEventListener("dragstart", dragstart);
+            turObject.addEventListener("dragend", dragend);
+        }
     }
 
-    finish();
-}
+    function finish() {
+        for (let turObject of turObjects) {
+            turObject.removeEventListener("dragstart", dragstart);
+            turObject.removeEventListener("dragend", dragend);
+        }
+        hideSidebar();
+        cancelChoiceButton.innerHTML = "Click aqui para cancelar la votación";
+        cancelChoiceButton.addEventListener("click", clearAll);
+        destination.addEventListener("click", clearAll);
+    }
 
-function hideSidebar() {
-    document.querySelector(".objects h3").style.display = "none";
-    document.querySelector(".objects").style.width = "0";
-    document.querySelector(".objects").style.padding = "0";
-    setTimeout(() => {
-        document.querySelector(".objects").style.display = "none";
-        document.querySelector("form").style.borderRadius = "10px";
-    }, 1000);
-}
+    function dragstart(event) {
+        event.target.classList.add("hold", "hanged");
+        objID = event.target.id;
 
-function showSidebar() {
-    turObjectContainer.style.display = "inherit";
-    document.querySelector(".objects h3").style.display = "inherit";
-    document.querySelector(".objects").style.width = "10rem";
-    document.querySelector(".objects").style.padding = "5px";
-    document.querySelector("form").style.borderRadius = "10px 0 0 10px";
+        destination.classList.add("available");
+    }
+
+    function dragend(event) {
+        event.target.classList.remove("hold");
+        destination.classList.remove("available");
+    }
+
+    function dragover(event) {
+        event.preventDefault();
+    }
+
+    function dragdrop(event) {
+        event.target.classList.remove("hovered");
+        document.getElementById(objID).classList.remove("hide");
+
+        event.target.append(document.getElementById(objID));
+        document.getElementById(objID).classList.add("inserted");
+        document.getElementById(objID).classList.remove("hold");
+        for (placeholder of placeholders) {
+            placeholder.classList.remove("available");
+        }
+
+        finish();
+    }
+
+    function hideSidebar() {
+        document.querySelector(".objects h3").style.display = "none";
+        document.querySelector(".objects").style.width = "0";
+        document.querySelector(".objects").style.padding = "0";
+        setTimeout(() => {
+            document.querySelector(".objects").style.display = "none";
+            document.querySelector("form").style.borderRadius = "10px";
+        }, 1000);
+    }
+
+    function showSidebar() {
+        turObjectContainer.style.display = "inherit";
+        document.querySelector(".objects h3").style.display = "inherit";
+        document.querySelector(".objects").style.width = "10rem";
+        document.querySelector(".objects").style.padding = "5px";
+        document.querySelector("form").style.borderRadius = "10px 0 0 10px";
+    }
 }
